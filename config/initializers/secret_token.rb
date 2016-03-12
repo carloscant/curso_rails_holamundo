f00f37f4edb95c11127b001e6789b1fde92767ca
Carlos Cantón <carloscant@gmail.com>
2016-03-12 12:15:22 +0100

    version 0

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Holamundo::Application.config.secret_token = '17d74d72bc796da13bc66a33b83ac2a1ca2fe30938a040cc539063f801d3e111ff5c4cdfc201e8b274737bccba1a3ddf1bb7504cd4cf859439ba765326fcadcf'
